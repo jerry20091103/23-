c461c94b1056e5f74836d2037817b8374d833d71
108062312 <plumy2828@gmail.com>
2022-08-07 20:34:00 +0800

    fix python

--- iris_hls/iris_python_v2.py
+++ iris_hls/iris_python_v2.py
@@ -6,7 +6,7 @@ from pynq import Overlay
 from pynq import allocate
 
 if __name__ == "__main__":
-    ol = Overlay("/home/xilinx/jupyter_notebooks/iris/iris_hls_v3.bit")
+    ol = Overlay("/home/xilinx/jupyter_notebooks/iris/iris_hls_v4.bit")
     ip_iris = ol.sw_compute_0
 
     # calculate the number of input data
@@ -21,16 +21,17 @@ if __name__ == "__main__":
     # allocate memory 
     inputmem = inputNum*4
     totalmem = inputNum*16
-    inBuffer = allocate(shape=(inputmem,), dtype=np.int32) 
-    outBuffer = allocate(shape=(totalmem,), dtype=np.int32)
-    outBufferPy = allocate(shape=(totalmem,), dtype=np.int32)
+    inBuffer = allocate(shape=(inputmem,), dtype=np.int8) 
+    outBuffer = allocate(shape=(inputNum,), dtype=np.int8)
+    outBufferPy = allocate(shape=(inputNum,), dtype=np.int8)
+    acc = [0]*totalmem
     
     # prepare input data
     Image.seek(0)
     for i in range(inputmem):
         line = Image.readline()
         inBuffer[i] = int(line)
-        outBufferPy[i] = int(line)
+        acc[i] = int(line)
     Image.close()
 
     # *start the computation for hls hardware
@@ -54,36 +55,38 @@ if __name__ == "__main__":
             112, -17, 24, -40, 98, 14, -2, -9, -44, -66, 2, -17, -49, -66, -8,
             -16, 3, 13, 82, 68, -128, -128, -126, 38, 37, 1, 29, 34, 4, 0, -19,
             29, 35, -4, -80, -66, 14, 19, 26, -45, -48, -3, -843, -91, 654 ]
-
+    
     # *start the computation for python
     timePythonStart = time()
+    #FC1
     for i in range(inputNum):
         for j in range(8):
             for k in range(4):
-                outBufferPy[inputNum*4+8*i+j] += outBufferPy[4*i+k]*weight[4*j+k] 
+                acc[inputNum*4+8*i+j] += acc[4*i+k]*weight[4*j+k] 
     for i in range(inputNum*4, inputNum*12):
         # Relu
-        if(outBufferPy[i]<0):
-            outBufferPy[i] = 0
+        if(acc[i]<0):
+            acc[i] = 0
         # Quan
-        outBufferPy[i] = outBufferPy[i]*scale_FC1 / 65536
-        if(outBufferPy[i]>127):
-            outBufferPy[i] = 127
+        acc[i] = acc[i]*scale_FC1 / 65536
+        if(acc[i]>127):
+            acc[i] = 127
+    #FC2
     for i in range(inputNum):
         for j in range(3):
-            outBufferPy[inputNum*12+3*i+j] += weight[56+j]
+            acc[inputNum*12+3*i+j] += weight[56+j]
             for k in range(8):
-                outBufferPy[inputNum*12+3*i+j] += outBufferPy[inputNum*4+8*i+k]*weight[32+8*j+k]
+                acc[inputNum*12+3*i+j] += acc[inputNum*4+8*i+k]*weight[32+8*j+k]
     #maxpooling
     for i in range(inputNum):
         #find max
-        max = outBufferPy[inputNum*12+3*i]
+        max = acc[inputNum*12+3*i]
         max_index = 0
         for j in range(3):
-            if(outBufferPy[inputNum*12+3*i+j] > max):
-                max = outBufferPy[inputNum*12+3*i+j]
+            if(acc[inputNum*12+3*i+j] > max):
+                max = acc[inputNum*12+3*i+j]
                 max_index = j
-        outBufferPy[inputNum*15+i] = max_index
+        outBufferPy[i] = max_index
     timePythonEnd = time()
     print("Python execution time: " + str(timePythonEnd - timePythonStart) + " s")
     
@@ -92,10 +95,8 @@ if __name__ == "__main__":
         print(">> results are the same!")
         print(">> the result:")
         for i in range(inputNum):
-            print(f"   image{i} : {outBuffer[inputNum*15+i]}")
+            print(f"   image{i} : {outBuffer[i]}")
     else:
         print("results are different!")
-        for i in range(inputNum*16):
+        for i in range(inputNum):
             print(f"index {i}: {outBuffer[i]} <-> {outBufferPy[i]}")
-            
-

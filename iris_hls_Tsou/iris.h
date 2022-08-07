10729c77664f822ae22b1a9f37135d14ca5474fd
108062312 <plumy2828@gmail.com>
2022-08-07 11:10:55 +0800

    Optimize main v2

--- iris_hls_Tsou/iris.h
+++ iris_hls_Tsou/iris.h
@@ -2,9 +2,9 @@
 #define __IRIS_H__
 
 
-
+#include "ap_int.h"
 typedef signed int int32_t;
-
-void iris_module(int32_t* act , int32_t* weight, int32_t scale_FC1, int32_t scale_FC2);
+typedef short int int16_t;
+void sw_compute(volatile ap_int<8>* im, volatile ap_int<3>* out);
 
 #endif

d5f5cadbbbdd5d8c9af593961e0e221cf2fe4a12
plumy2828 <plumy2828@gmail.com>
2022-09-08 22:22:09 +0800

    整理function name

--- R2+1D/r2plus1d.cpp
+++ R2+1D/r2plus1d.cpp
@@ -1,14 +1,18 @@
 #include<iostream>
 using namespace std;
 
-void r2plus1d(DATA_TYPE X_in(N1, C1, D1, H1, W1))
+void r2plus1d(Array_5D X_in, Array_5D Conv3d_Kernel_1, Array_5D Conv3d_Kernel_2)
 {
 	
-	└─Conv3d: 2-1                            [1, 45, 1, 56, 56]        6,615
-    └─BatchNorm3d: 2-2                       [1, 45, 1, 56, 56]        90
-    └─ReLU: 2-3                              [1, 45, 1, 56, 56]        --
-    └─Conv3d: 2-4                            [1, 64, 1, 56, 56]        8,640
-    └─BatchNorm3d: 2-5                       [1, 64, 1, 56, 56]        128
-    └─ReLU: 2-6 
+    Conv3d(Array_5D X_in, Array_5D X_out, Array_5D Kernel, int* stride, int* padding);
+    BatchNorm3d(Array_5D X, int e = 0.00001, int r = 1, int b = 0);
+    ReLU(Array_5D X);
+
+	// └─Conv3d: 2-1                            [1, 45, 1, 56, 56]        6,615
+    // └─BatchNorm3d: 2-2                       [1, 45, 1, 56, 56]        90
+    // └─ReLU: 2-3                              [1, 45, 1, 56, 56]        --
+    // └─Conv3d: 2-4                            [1, 64, 1, 56, 56]        8,640
+    // └─BatchNorm3d: 2-5                       [1, 64, 1, 56, 56]        128
+    // └─ReLU: 2-6 
 
 }
--- R2+1D/r2plus1d.h
+++ R2+1D/r2plus1d.h
@@ -11,10 +11,10 @@ typedef struct Array_5D_ {
     int *num;
 } Array_5D;
 
-void r2plus1d(volatile ap_int<8>* im, volatile ap_int<3>* out);
+void r2plus1d(Array_5D X_in, Array_5D Conv3d_Kernel_1, Array_5D Conv3d_Kernel_2);
 void Conv3d(Array_5D X_in, Array_5D X_out, Array_5D Kernel, int* stride, int* padding);
-void BatchNorm3d(DATA_TYPE X(N, C, D, H, W), int e = 0.00001, int r = 1, int b = 0);
-void ReLU(DATA_TYPE X(N, C, D, H, W));
+void BatchNorm3d(Array_5D X, int e = 0.00001, int r = 1, int b = 0);
+void ReLU(Array_5D X);
 void Sequential(psum_range, x, weights, out_channels, kernel_size=5, stride=1, bias=False, psum_record=False);
 
 #endif

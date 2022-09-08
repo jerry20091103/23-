0ca612d92cd6c378d49f56e6d77d8424578ebfb7
108062235 <s108062235@m108.nthu.edu.tw>
2022-09-08 22:08:37 +0800

    change data type

--- R2+1D/ReLU.cpp
+++ R2+1D/ReLU.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 using namespace std;
 
-void ReLU(DATA_TYPE X(N, C, D, H, W)){
-	
-    for(int n = 0; n < N; n++){
+void ReLU(Array_5D X){
+	int N = X_in.num[0];
+	int C = X_in.num[1];
+	int D = X_in.num[2];
+	int H = X_in.num[3];
+	int W = X_in.num[4];
+
+	for(int n = 0; n < N; n++){
 		for(int c = 0; c < C; c++){
 			for(int d = 0; d < D; d++){
 				for(int h = 0; h < H; h++){
 					for(int w = 0; w < W; w++){
-						X[n][c][d][h][w] = (X[n][c][d][h][w] > 0) ? X[n][c][d][h][w] : 0;
+						int pos = n*C*D*H*W + c*D*H*W + d*H*W + h*W + w;
+						X[pos] = (X[pos] > 0) ? X[pos] : 0;
 					}
 				}
 			}

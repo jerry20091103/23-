278f8bdb1e0018b7b0364773a406c295d8b3f6c8
plumy2828 <plumy2828@gmail.com>
2022-09-08 21:40:46 +0800

    modify .h file

--- R2+1D/r2plus1d.h
+++ R2+1D/r2plus1d.h
@@ -7,8 +7,8 @@ typedef signed int int32_t;
 typedef short int int16_t;
 
 typedef struct Array_5D_ {
-    float data[];
-    int num;
+    int *data;
+    int *num;
 } Array_5D;
 
 void r2plus1d(volatile ap_int<8>* im, volatile ap_int<3>* out);

44555127ecde673b560d697348987711469b81b3
plumy2828 <plumy2828@gmail.com>
2022-09-08 21:21:45 +0800

    plus .h file

---
+++ R2+1D/r2plus1d.h
@@ -0,0 +1,16 @@
+#ifndef __R2PLUS1D_H__
+#define __R2PLUS1D_H__
+
+
+#include "ap_int.h"
+typedef signed int int32_t;
+typedef short int int16_t;
+
+typedef struct Array_5D_ {
+    float data[];
+    int num;
+} Array_5D;
+
+void r2plus1d(volatile ap_int<8>* im, volatile ap_int<3>* out);
+
+#endif

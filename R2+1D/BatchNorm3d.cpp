d18b536bbb414127d489bd3182b82e18766fd9cf
108062235 <s108062235@m108.nthu.edu.tw>
2022-09-30 21:26:36 +0800

    include math.h

--- R2+1D/BatchNorm3d.cpp
+++ R2+1D/BatchNorm3d.cpp
@@ -1,56 +1,9 @@
 #include "r2plus1d.h"
 #include<iostream>
+#include <math.h>
 using namespace std;
 
-#define _C 512
-
-// source: https://www.codeproject.com/Articles/570700/SquareplusRootplusalgorithmplusforplusC
-double powerOfTen(int num){
-    double rst = 1.0;
-    if(num >= 0)
-        for(int i = 0; i < num ; i++)
-            rst *= 10.0;
-    else
-        for(int i = 0; i < (0 - num ); i++)
-           rst *= 0.1;
-   return rst;
-}
-
-double sqrt(double a)
-{
-    double z = a; 
-    double rst = 0.0;
-    int max = 8;     // to define maximum digit
-    double j = 1.0;
-
-    for(int i = max ; i > 0 ; i--){
-        // value must be bigger then 0
-        double pow_i = powerOfTen(i);
-        if(z - (2*rst + j*pow_i) * j * pow_i >= 0){
-            while(z - (2*rst + j*pow_i) * j * pow_i >= 0)
-                if(++j >= 10)
-                    break;
-            j--; //correct the extra value by minus one to j
-            z -= (2*rst + j*pow_i) * j * pow_i; //find value of z
-            rst += j * pow_i;     // find sum of a
-            j = 1.0;
-        }
-    }
-
-    for(int i = 0 ; i >= 0 - max ; i--){
-        double pow_i = powerOfTen(i);
-        if(z - (2*rst + j*pow_i) * j * pow_i >= 0){
-            while(z - (2*rst + j*pow_i) * j * pow_i >= 0)
-                j++;
-            j--;
-            z -= (2*rst + j*pow_i) * j * pow_i; //find value of z
-            rst += j * pow_i; // find sum of a
-            j = 1.0;
-        }
-    }
-    // find the number on each digit
-    return rst;
-}
+#define _C 1152
 
 void BatchNorm3d(float* X_data, int* X_num, float e, int r, int b){
 	int N = X_num[0];

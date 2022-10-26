1a3f88d823b2d61a2b7c2c7ce1d780752e35f01b
plumy2828 <plumy2828@gmail.com>
2022-10-26 21:37:33 +0800

    change data type

--- R2+1D/r2plus1d.cpp
+++ R2+1D/r2plus1d.cpp
@@ -2,58 +2,55 @@
 #include<iostream>
 using namespace std;
 
-void r2plus1d(double* X_data, double* Y_data, double* Kernel_1_data, double* Kernel_2_data)
+void r2plus1d(dtype* X_data, dtype* Y_data, dtype* Kernel_1_data, dtype* Kernel_2_data)
 {
  #ifdef __SYNTHESIS__
-    double X_out_data[141120];
-    double Y_tmp_data[200704];
+    dtype X_out_data[141120];
+    dtype Y_tmp_data[200704];
 
-    #pragma HLS INTERFACE s_axilite port=return
-	#pragma HLS INTERFACE m_axi port=X_data
-	#pragma HLS INTERFACE m_axi port=Y_data
-    #pragma HLS INTERFACE m_axi port=Kernel_1_data
-	#pragma HLS INTERFACE m_axi port=Kernel_2_data
+    #pragma HLS int_tERFACE s_axilite port=return
+	#pragma HLS int_tERFACE m_axi port=X_data
+	#pragma HLS int_tERFACE m_axi port=Y_data
+    #pragma HLS int_tERFACE m_axi port=Kernel_1_data
+	#pragma HLS int_tERFACE m_axi port=Kernel_2_data
  #else
-     double* X_out_data = (double*)malloc(141120*sizeof(double)); // value after first Conv-Batch-ReLU layer
-     double* Y_tmp_data = (double*)malloc(200704*sizeof(double)); // value after second Conv-Batch-ReLU layer
+     dtype* X_out_data = (dtype*)malloc(141120*sizeof(dtype)); // value after first Conv-Batch-ReLU layer
+     dtype* Y_tmp_data = (dtype*)malloc(200704*sizeof(dtype)); // value after second Conv-Batch-ReLU layer
  #endif
 
-    // R2Plus1dStem
-	int X_num[5] = {1, 3, 1, 111, 111};
-    int X_out_num[5] = {1, 45, 1, 56, 56};
-    int Kernel_1_num[3] = {1, 7, 7};
-    int stride_1[3] = {1, 2, 2};
-    int padding_1[3] = {0, 3, 3};
-
-    // // for detail test
-    // for(int i = 0; i < 141120; i++)
-    //     X_out_data[i] = X_data[i]; // assign result to output
+    // ========================R2Plus1dStem ==================================
+	int_t X_num[5] = {1, 3, 1, 111, 111};
+    int_t X_out_num[5] = {1, 45, 1, 56, 56};
+    int_t Kernel_1_num[3] = {1, 7, 7};
+    int_t stride_1[3] = {1, 2, 2};
+    int_t padding_1[3] = {0, 3, 3};
 
     Conv3d(X_data, X_num, X_out_data, X_out_num, Kernel_1_data, Kernel_1_num, stride_1, padding_1);
     BatchNorm3d(X_out_data, X_out_num, 0.00001, 1, 0);
     ReLU(X_out_data, X_out_num);
     
-    // // for detail test
-    // for(int i = 0; i < 141120; i++)
-    //     Y_data[i] = X_out_data[i]; // assign result to output
 
-    int Y_num[5] = {1, 64, 1, 56, 56};
-    int Kernel_2_num[3] = {3, 1, 1};
-    int stride_2[3] = {1, 1, 1};
-    int padding_2[3] = {1, 0, 0};
+    int_t Y_num[5] = {1, 64, 1, 56, 56};
+    int_t Kernel_2_num[3] = {3, 1, 1};
+    int_t stride_2[3] = {1, 1, 1};
+    int_t padding_2[3] = {1, 0, 0};
 
     Conv3d(X_out_data, X_out_num, Y_tmp_data, Y_num, Kernel_2_data, Kernel_2_num, stride_2, padding_2);
     BatchNorm3d(Y_tmp_data, Y_num, 0.00001, 1, 0);
     ReLU(Y_tmp_data, Y_num);
     
-    for(int i = 0; i < 200704; i++)
+    for(int_t i = 0; i < 200704; i++)
         Y_data[i] = Y_tmp_data[i]; // assign result to output
+
+    // ========================R2Plus1dStem end ==================================
     
-    // ==========================================================
+    
+    
+    // ========================Sequential 1~4==================================
 
-    // Sequential 1~4
+    // 
 
-    // ==========================================================
+    // ========================Sequential 1~4 end==============================
  #ifndef __SYNTHESIS__
     free(X_out_data);
     free(Y_tmp_data);

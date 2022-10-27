eef7d2f40a4508e5d8f62f72cf4c5a936f413538
plumy2828 <plumy2828@gmail.com>
2022-10-28 01:22:24 +0800

    #define _C, scale, zeropoint in Linear.cpp

--- R2+1D/Linear.cpp
+++ R2+1D/Linear.cpp
@@ -3,19 +3,21 @@
 #include <cmath>
 using namespace std; 
 
-// Y_num(out_features) = 10, scale = 3.984360396862030029e-02, zeropoint = 127
+#define _C 10  // Y_num(out_features) = 10
+#define scale 3.984360396862030029e-02
+#define zeropoint 127
+
 void Linear(dtype* X_data, int_t* X_num, dtype* Y_data, dtype* Kernel){
     int H = X_num[0];
     int W = X_num[1];
-    int C = 10;
     
-    for(int i = 0; i < C*H; i++)
+    for(int i = 0; i < _C*H; i++)
         Y_data[i] = 0;
 
     for(int h = 0; h < H; h++)
-        for(int c = 0; c < C; c++)
+        for(int c = 0; c < _C; c++)
             for(int i = 0; i < W; i++)
-                Y_data[h*C+c] += round( (Kernel[c*W+i]*X_data[h*W+i])/3.984360396862030029e-02 + 127 );
+                Y_data[h*_C+c] += round( (Kernel[c*W+i]*X_data[h*W+i])/scale + zeropoint );
     
     return;
 }

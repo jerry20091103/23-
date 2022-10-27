14ced34eddb55b047ee1fe9651ef9bc3ff2114c1
jerry20091103 <jerry20091103@gmail.com>
2022-10-27 21:14:37 +0800

    Fix bug in Linear; Add Linear testbench.

--- R2+1D/Linear.cpp
+++ R2+1D/Linear.cpp
@@ -5,7 +5,14 @@ using namespace std;
 void Linear(dtype* X_data, int_t* X_num, dtype* Y_data, int_t* Y_num, dtype* Kernel){
     int H = X_num[0];
     int W = X_num[1];
-    int C = Y_num[0];
+    int C = Y_num[1];
+
+    // initialize Y_data
+    for(int c = 0; c < C; c++){
+        for(int h = 0; h < H; h++){
+            Y_data[h*c] = 0;
+        }
+    }
 
     for(int h = 0; h < H; h++)
         for(int c = 0; c < C; c++)

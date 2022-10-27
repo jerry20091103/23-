033eece6bf8ee5ddc7d94389e95786096ab83c1b
plumy2828 <plumy2828@gmail.com>
2022-10-27 20:13:17 +0800

    Linear.cpp修正完成

--- R2+1D/Linear.cpp
+++ R2+1D/Linear.cpp
@@ -1,8 +1,9 @@
 #include "r2plus1d.h"
 #include<iostream>
+#include <cmath>
 using namespace std;
 
-void Linear(dtype* X_data, int_t* X_num, dtype* Y_data, int_t* Y_num, dtype* Kernel){
+void Linear(dtype* X_data, int_t* X_num, dtype* Y_data, int_t* Y_num, dtype* Kernel, double scale, int_t zeropoint){
     int H = X_num[0];
     int W = X_num[1];
     int C = Y_num[0];
@@ -10,8 +11,7 @@ void Linear(dtype* X_data, int_t* X_num, dtype* Y_data, int_t* Y_num, dtype* Ker
     for(int h = 0; h < H; h++)
         for(int c = 0; c < C; c++)
             for(int i = 0; i < W; i++)
-                Y_data[h*C+c] += Kernel[c*W+i] * X_data[h*W+i];
-        
+                Y_data[h*C+c] += round( (Kernel[c*W+i]*X_data[h*W+i])/scale + zeropoint );
     return;
 }
 

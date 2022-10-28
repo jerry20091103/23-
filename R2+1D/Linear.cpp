c9f51e38f49bb023e36bd5fb40d831f8b289a7fb
plumy2828 <plumy2828@gmail.com>
2022-10-28 19:25:42 +0800

    修正Linear.cpp #define _C改成叫out_features

--- R2+1D/Linear.cpp
+++ R2+1D/Linear.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std; 
 
-#define _C 10  // Y_num(out_features) = 10
+#define out_features 10  // Y_num(out_features) = 10
 #define scale 3.984360396862030029e-02
 #define zeropoint 127
 
@@ -11,13 +11,13 @@ void Linear(dtype* X_data, int_t* X_num, dtype* Y_data, dtype* Kernel){
     int H = X_num[0];
     int W = X_num[1];
     
-    for(int i = 0; i < _C*H; i++)
+    for(int i = 0; i < out_features*H; i++)
         Y_data[i] = 0;
 
     for(int h = 0; h < H; h++)
-        for(int c = 0; c < _C; c++)
+        for(int c = 0; c < out_features; c++)
             for(int i = 0; i < W; i++)
-                Y_data[h*_C+c] += round( (Kernel[c*W+i]*X_data[h*W+i])/scale + zeropoint );
+                Y_data[h*out_features+c] += round( (Kernel[c*W+i]*X_data[h*W+i])/scale + zeropoint );
     
     return;
 }

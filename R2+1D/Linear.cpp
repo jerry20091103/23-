23e4f24e3d761e0a0a3a28d1d6f2b536b00c88c4
plumy2828 <plumy2828@gmail.com>
2022-11-01 22:53:11 +0800

    修正linear.cpp裡面quantize寫法

--- R2+1D/Linear.cpp
+++ R2+1D/Linear.cpp
@@ -4,21 +4,30 @@
 using namespace std; 
 
 #define out_features 10  // Y_num(out_features) = 10
-#define scale 3.984360396862030029e-02
-#define zeropoint 127
 
-void Linear(dtype* X_data, int_t* X_num, dtype* Y_data, dtype* Kernel){
+#define scale_in  1.026933342218399048e-01
+#define zp_in 42
+#define scale_out 3.984360396862030029e-02
+#define zp_out 127
+
+
+
+void Linear(dtype* X_data, int_t* X_num, dtype* Y_data, dtype* Kernel, double* kernel_scale){
     int H = X_num[0];
     int W = X_num[1];
     
+    // dequan X
+	for(int_t i = H*W-1; i >= 0; i--)
+		X_data[i] -= zp_in;
+
+    // initial Y
     for(int i = 0; i < out_features*H; i++)
         Y_data[i] = 0;
 
     for(int h = 0; h < H; h++)
         for(int c = 0; c < out_features; c++)
             for(int i = 0; i < W; i++)
-                Y_data[h*out_features+c] += round( (Kernel[c*W+i]*X_data[h*W+i])/scale + zeropoint );
-    
+                Y_data[h*out_features+c] += round( (Kernel[c*W+i]*X_data[h*W+i]*scale_in*kernel_scale[c])/scale_out + zp_out);
     return;
 }
 

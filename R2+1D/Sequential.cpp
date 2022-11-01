1bd868a22f92a06827dfc59919ea0327e9b5f5e8
108062235 <s108062235@m108.nthu.edu.tw>
2022-11-01 21:51:22 +0800

    add ReLU zeropoint

--- R2+1D/Sequential.cpp
+++ R2+1D/Sequential.cpp
@@ -62,7 +62,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
                 X_scale, X_zeropoint, 8.706942945718765259e-02, 4.961582273244857788e-02, 64, 71,
                 Mu_seq_1_1, Var_seq_1_1, Gamma_seq_1_1, bias_seq_1_1, 4.489336907863616943e-02, 60); // 144 = (inplanes * planes * 3 * 3) / (inplanes * 3 + planes)
     BatchNorm3d(X_data, X_num, Mu_seq_1_2, Var_seq_1_2, Gamma_seq_1_2, bias_seq_1_2, 5.436319485306739807e-02, 74);
-    ReLU(X_data, X_num);
+    ReLU(X_data, X_num, 74);
     Conv2Plus1D(X_data, X_num, X_data, X_num, 144, 
                 Kernel_1_3_data, Kernel_1_4_data, Kernel_1_3_data_scale, Kernel_1_4_data_scale, 1, 1, 
                 5.436319485306739807e-02, 74, 6.804036349058151245e-02, 3.850702568888664246e-02, 60, 66,
@@ -70,7 +70,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
     BatchNorm3d(X_data, X_num, Mu_seq_1_4, Var_seq_1_4, Gamma_seq_1_4, bias_seq_1_4, 4.517441987991333008e-02, 68);
     for(int_t i = 0; i < 3211264; i++)
         X_data[i] += X_tmp_data[i];
-    ReLU(X_data, X_num);
+    ReLU(X_data, X_num, 68);
 
     for(int_t i = 0; i < 3211264; i++)
         X_tmp_data[i] = X_data[i];
@@ -79,7 +79,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
                 4.517441987991333008e-02, 68, 9.410868585109710693e-02, 3.386811539530754089e-02, 72, 67,
                 Mu_seq_1_5,Var_seq_1_5, Gamma_seq_1_5, bias_seq_1_5, 3.406318649649620056e-02, 76); // 144 = (inplanes * planes * 3 * 3) / (inplanes * 3 + planes)
     BatchNorm3d(X_data, X_num, Mu_seq_1_6, Var_seq_1_6, Gamma_seq_1_6, bias_seq_1_6, 4.148417711257934570e-02, 70);
-    ReLU(X_data, X_num);
+    ReLU(X_data, X_num, 70);
     Conv2Plus1D(X_data, X_num, X_data, X_num, 144, 
                 Kernel_1_7_data, Kernel_1_8_data, Kernel_1_7_data_scale, Kernel_1_8_data_scale, 1, 1, 
                 4.148417711257934570e-02, 70, 3.422784805297851562e-02, 2.731916867196559906e-02, 68, 71,
@@ -88,7 +88,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
     BatchNorm3d(X_data, X_num, Mu_seq_1_8, Var_seq_1_8, Gamma_seq_1_8, bias_seq_1_8, 5.917721241712570190e-02, 53);
     for(int_t i = 0; i < 3211264; i++)
         X_data[i] += X_tmp_data[i];
-    ReLU(X_data, X_num);
+    ReLU(X_data, X_num, 53);
 
     // // =============================================================================
     // // layer 2
@@ -104,7 +104,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
 
     BatchNorm3d(X2_data, X2_num, Mu_seq_2_2, Var_seq_2_2, Gamma_seq_2_2, bias_seq_2_2, 3.730613738298416138e-02, 52);
     
-    ReLU(X2_data, X2_num);
+    ReLU(X2_data, X2_num, 52);
     Conv2Plus1D(X2_data, X2_num, X2_data, X2_num, 230, 
                 Kernel_2_3_data, Kernel_2_4_data, Kernel_2_3_data_scale, Kernel_2_4_data_scale, 1, 1,
                 3.730613738298416138e-02, 52, 6.581791490316390991e-02, 3.792280331254005432e-02, 68, 70,
@@ -116,7 +116,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
    
     for(int_t i = 0; i< 802816; i++)
         X2_data[i] += X2_tmp_data[i];
-    ReLU(X2_data, X2_num);
+    ReLU(X2_data, X2_num, 53);
     
     for(int_t i = 0; i < 802816; i++)
         X2_tmp_data[i] = X2_data[i];
@@ -126,7 +126,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
                 Mu_seq_2_6,Var_seq_2_6, Gamma_seq_2_6, bias_seq_2_6, 2.571923658251762390e-02, 74); // 288 = (128 * 128 * 3 * 3) / (128 * 3 + 128)
     BatchNorm3d(X2_data, X2_num, Mu_seq_2_7, Var_seq_2_7, Gamma_seq_2_7, bias_seq_2_7, 4.108780622482299805e-02, 69);
     
-    ReLU(X2_data, X2_num);
+    ReLU(X2_data, X2_num, 69);
     Conv2Plus1D(X2_data, X2_num, X2_data, X2_num, 288, 
                 Kernel_2_8_data, Kernel_2_9_data, Kernel_2_8_data_scale, Kernel_2_9_data_scale, 1, 1,
                 4.108780622482299805e-02, 69, 4.689884185791015625e-02, 2.438377402722835541e-02, 55, 66,
@@ -134,7 +134,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
     BatchNorm3d(X2_data, X2_num, Mu_seq_2_9, Var_seq_2_9, Gamma_seq_2_9, bias_seq_2_9, 6.300298124551773071e-02, 70);
     for(int_t i = 0; i < 802816; i++)
         X2_data[i] += X2_tmp_data[i];
-    ReLU(X2_data, X2_num);
+    ReLU(X2_data, X2_num, 70);
 
     // // =============================================================================
     // // layer 3
@@ -147,7 +147,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
                 6.300298124551773071e-02, 70, 1.026936098933219910e-01, 3.513325005769729614e-02, 66, 64,
                 Mu_seq_3_1, Var_seq_3_1, Gamma_seq_3_1, bias_seq_3_1, 3.207130357623100281e-02, 55); // 460 = (128 * 256 * 3 * 3) / (128 * 3 + 256)
     BatchNorm3d(X3_data, X3_num, Mu_seq_3_2, Var_seq_3_2, Gamma_seq_3_2, bias_seq_3_2, 3.827788308262825012e-02, 59);
-    ReLU(X3_data, X3_num);
+    ReLU(X3_data, X3_num, 59);
     Conv2Plus1D(X3_data, X3_num, X3_data, X3_num, 460, 
                 Kernel_3_3_data, Kernel_3_4_data, Kernel_3_3_data_scale, Kernel_3_4_data_scale, 1, 1,
                 3.827788308262825012e-02, 59, 7.229902595281600952e-02, 3.355694189667701721e-02, 74, 57,
@@ -158,7 +158,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
     BatchNorm3d(X3_tmp_data, X3_num, Mu_seq_3_5, Var_seq_3_5, Gamma_seq_3_5, bias_seq_3_5, 4.242179170250892639e-02, 52);
     for(int_t i = 0; i< 200704; i++)
         X3_data[i] += X3_tmp_data[i];
-    ReLU(X3_data, X3_num);
+    ReLU(X3_data, X3_num, 52);
     
     for(int_t i = 0; i< 200704; i++)
         X3_tmp_data[i] = X3_data[i];
@@ -167,7 +167,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
                 4.242179170250892639e-02, 52, 9.108851104974746704e-02, 2.204562351107597351e-02, 60, 64,
                 Mu_seq_3_6, Var_seq_3_6, Gamma_seq_3_6, bias_seq_3_6, 1.979854144155979156e-02, 64);
     BatchNorm3d(X3_data, X3_num, Mu_seq_3_7, Var_seq_3_7, Gamma_seq_3_7, bias_seq_3_7, 2.682571485638618469e-02, 55);
-    ReLU(X3_data, X3_num);
+    ReLU(X3_data, X3_num, 55);
     Conv2Plus1D(X3_data, X3_num, X3_data, X3_num, 576, 
                 Kernel_3_8_data, Kernel_3_9_data, Kernel_3_8_data_scale, Kernel_3_9_data_scale, 1, 1,
                 2.682571485638618469e-02, 55, 4.488958418369293213e-02, 3.477662429213523865e-02, 58, 82,
@@ -175,7 +175,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
     BatchNorm3d(X3_data, X3_num, Mu_seq_3_9, Var_seq_3_9, Gamma_seq_3_9, bias_seq_3_9, 3.744378685951232910e-02, 78);
     for(int_t i = 0; i< 200704; i++)
         X3_data[i] += X3_tmp_data[i];
-    ReLU(X3_data, X3_num);
+    ReLU(X3_data, X3_num, 78);
 
     // // =============================================================================
 
@@ -189,7 +189,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
                 3.744378685951232910e-02, 78, 7.933901995420455933e-02, 4.174583032727241516e-02, 64, 60,
                 Mu_seq_4_1, Var_seq_4_1, Gamma_seq_4_1, bias_seq_4_1, 2.300033532083034515e-02, 71); // 921 = (256 * 512 * 3 * 3) / (256 * 3 + 512)
     BatchNorm3d(X4_data, X4_num, Mu_seq_4_2, Var_seq_4_2, Gamma_seq_4_2, bias_seq_4_2, 3.296769410371780396e-02, 49);
-    ReLU(X4_data, X4_num);
+    ReLU(X4_data, X4_num, 49);
     Conv2Plus1D(X4_data, X4_num, X4_data, X4_num, 921, 
                 Kernel_4_3_data, Kernel_4_4_data, Kernel_4_3_data_scale, Kernel_4_4_data_scale, 1, 1,
                 3.296769410371780396e-02, 49, 5.654629692435264587e-02, 3.702012076973915100e-02, 61, 56,
@@ -200,7 +200,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
     BatchNorm3d(X4_tmp_data, X4_num, Mu_seq_4_5, Var_seq_4_5, Gamma_seq_4_5, bias_seq_4_5, 5.310279503464698792e-02, 59);
     for(int_t i = 0; i< 50176; i++)
         X4_data[i] += X4_tmp_data[i];
-    ReLU(X4_data, X4_num);
+    ReLU(X4_data, X4_num, 59);
     
     for(int_t i = 0; i< 50176; i++)
         X4_tmp_data[i] = X4_data[i];
@@ -209,7 +209,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
                 5.083529949188232422e-01, 3.209327906370162964e-02, 57, 59,
                 8.961183577775955200e-02, 57, Mu_seq_4_6, Var_seq_4_6, Gamma_seq_4_6, bias_seq_4_6, 2.375184185802936554e-02, 66); // 1152 = (512 * 512 * 3 * 3) / (512 * 3 + 512)
     BatchNorm3d(X4_data, X4_num, Mu_seq_4_7, Var_seq_4_7, Gamma_seq_4_7, bias_seq_4_7, 2.595668099820613861e-02, 68);
-    ReLU(X4_data, X4_num);
+    ReLU(X4_data, X4_num, 68);
     Conv2Plus1D(X4_data, X4_num, X4_data, X4_num, 1152, 
                 Kernel_4_8_data, Kernel_4_9_data, Kernel_4_8_data_scale, Kernel_4_9_data_scale, 1, 1,
                 2.595668099820613861e-02, 68, 8.170315623283386230e-02, 2.505685016512870789e-02, 67, 59,
@@ -217,7 +217,7 @@ void Sequential(dtype* X_data, dtype* Y_data,
     BatchNorm3d(X4_data, X4_num, Mu_seq_4_9, Var_seq_4_9, Gamma_seq_4_9, bias_seq_4_9, 1.026933342218399048e-01, 42);
     for(int_t i = 0; i< 50176; i++)
         X4_data[i] += X4_tmp_data[i];
-    ReLU(X4_data, X4_num);
+    ReLU(X4_data, X4_num, 42);
 
     // =============================================================================
 

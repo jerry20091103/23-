edbaaea66b058a32d904492f1dc908b51ac8d3e5
plumy2828 <plumy2828@gmail.com>
2022-11-01 19:24:16 +0800

    嘗試改batchNorm3d寫法

--- R2+1D/BatchNorm3d.cpp
+++ R2+1D/BatchNorm3d.cpp
@@ -15,36 +15,37 @@ void BatchNorm3d(dtype* X_data, int_t* X_num, double* mu_, double* var_, double*
 	for(int_t i = N*C*D*H*W-1; i >= 0; i--)
 		X_data[i] -= zp_in;
 	
-	// find channel mean
-	double mu[C_];
-    for(int_t c = 0; c < C; c++){
-	    for(int_t n = 0; n < N; n++){
-			mu[c] = 0;
-			for(int_t d = 0; d < D; d++)
-				for(int_t h = 0; h < H; h++)
-					for(int_t w = 0; w < W; w++)
-						mu[c] += X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w];
-			mu[c] /= D*H*W;
-		}
-	}
+	// // find channel mean
+	// double mu[C_];
+    // for(int_t c = 0; c < C; c++){
+	//     for(int_t n = 0; n < N; n++){
+	// 		mu[c] = 0;
+	// 		for(int_t d = 0; d < D; d++)
+	// 			for(int_t h = 0; h < H; h++)
+	// 				for(int_t w = 0; w < W; w++)
+	// 					mu[c] += X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w];
+	// 		mu[c] /= D*H*W;
+	// 	}
+	// }
 
-    // find channel variance
-	double var[C_];
-    for(int_t c = 0; c < C; c++){
-	    for(int_t n = 0; n < N; n++){
-			var[c] = 0;
-			for(int_t d = 0; d < D; d++)
-				for(int_t h = 0; h < H; h++)
-					for(int_t w = 0; w < W; w++)
-						var[c] += (X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w]-mu[c]) * (X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w]-mu[c]);
-			var[c] /= D*H*W;
-		}
-	}
+    // // find channel variance
+	// double var[C_];
+    // for(int_t c = 0; c < C; c++){
+	//     for(int_t n = 0; n < N; n++){
+	// 		var[c] = 0;
+	// 		for(int_t d = 0; d < D; d++)
+	// 			for(int_t h = 0; h < H; h++)
+	// 				for(int_t w = 0; w < W; w++)
+	// 					var[c] += (X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w]-mu[c]) * (X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w]-mu[c]);
+	// 		var[c] /= D*H*W;
+	// 	}
+	// }
 
 	for(int_t n = 0; n < N; n++)
 		for(int_t c = 0; c < C; c++)
 			for(int_t d = 0; d < D; d++)
 				for(int_t h = 0; h < H; h++)
 					for(int_t w = 0; w < W; w++)
-						X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w] = round((((float(X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w])*scale_in - (0.1*mu[c]*scale_in+0.9*mu_[c])) / sqrt((0.1*var[c]*pow(scale_in, 2)+0.9*var_[c])+0.00001)) * r[c] + b[c])/scale_out + zp_out);
+						X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w] = round((((float(X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w])*scale_in - mu_[c]) / sqrt(var_[c]+0.00001)) * r[c] + b[c])/scale_out + zp_out);
+						// X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w] = round((((float(X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w])*scale_in - (0.1*mu[c]*scale_in+0.9*mu_[c])) / sqrt((0.1*var[c]*pow(scale_in, 2)+0.9*var_[c])+0.00001)) * r[c] + b[c])/scale_out + zp_out);
 }

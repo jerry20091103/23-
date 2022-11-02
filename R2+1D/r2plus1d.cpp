3cd5e371ce6b7544a587ff5721428d0e1de262f5
108062235 <s108062235@m108.nthu.edu.tw>
2022-11-02 21:59:25 +0800

    test sequential layer

--- R2+1D/r2plus1d.cpp
+++ R2+1D/r2plus1d.cpp
@@ -244,31 +244,31 @@ void r2plus1d(dtype* X, dtype* Y, dtype* Kernel_stem_0, dtype* Kernel_stem_3, do
  #endif
 
     // ========================R2Plus1dStem ==================================
-	int_t X_num[5] = {N_, 3, D_, 112, 112};
-    int_t X_stem_1_num[5] = {N_, 45, D_, 56, 56};
-    int_t Kernel_stem_1_num[3] = {1, 7, 7};
-    int_t stride_1[3] = {1, 2, 2};
-    int_t padding_1[3] = {0, 3, 3};
+	// int_t X_num[5] = {N_, 3, D_, 112, 112};
+    // int_t X_stem_1_num[5] = {N_, 45, D_, 56, 56};
+    // int_t Kernel_stem_1_num[3] = {1, 7, 7};
+    // int_t stride_1[3] = {1, 2, 2};
+    // int_t padding_1[3] = {0, 3, 3};
 
-    Conv3d(X, X_num, X_stem_1, X_stem_1_num, Kernel_stem_0, Kernel_stem_1_num, Kernel_stem_0_scale, stride_1, padding_1, 3.756307810544967651e-02, 56, 0.4609071612358093262, 60);
-    BatchNorm3d(X_stem_1, X_stem_1_num, Mu_stem_1, Var_stem_1, Gamma_stem_1, Bias_stem_1, 0.4609071612358093262, 60, 0.07323520630598068237, 55);
-    ReLU(X_stem_1, X_stem_1_num, 55);
+    // Conv3d(X, X_num, X_stem_1, X_stem_1_num, Kernel_stem_0, Kernel_stem_1_num, Kernel_stem_0_scale, stride_1, padding_1, 3.756307810544967651e-02, 56, 0.4609071612358093262, 60);
+    // BatchNorm3d(X_stem_1, X_stem_1_num, Mu_stem_1, Var_stem_1, Gamma_stem_1, Bias_stem_1, 0.4609071612358093262, 60, 0.07323520630598068237, 55);
+    // ReLU(X_stem_1, X_stem_1_num, 55);
 
-    int_t X_stem_2_num[5] = {N_, 64, D_, 56, 56};
-    int_t Kernel_stem_2_num[3] = {3, 1, 1};
-    int_t stride_2[3] = {1, 1, 1};
-    int_t padding_2[3] = {1, 0, 0};
+    // int_t X_stem_2_num[5] = {N_, 64, D_, 56, 56};
+    // int_t Kernel_stem_2_num[3] = {3, 1, 1};
+    // int_t stride_2[3] = {1, 1, 1};
+    // int_t padding_2[3] = {1, 0, 0};
 
-    Conv3d(X_stem_1, X_stem_1_num, X_stem_2, X_stem_2_num, Kernel_stem_3, Kernel_stem_2_num, Kernel_stem_3_scale, stride_2, padding_2, 0.07323520630598068237, 55, 0.09311912953853607178, 70);
-    BatchNorm3d(X_stem_2, X_stem_2_num, Mu_stem_4, Var_stem_4, Gamma_stem_4, Bias_stem_4, 0.09311912953853607178, 70, 0.07423608750104904175, 65);
-    ReLU(X_stem_2, X_stem_2_num, 65);
+    // Conv3d(X_stem_1, X_stem_1_num, X_stem_2, X_stem_2_num, Kernel_stem_3, Kernel_stem_2_num, Kernel_stem_3_scale, stride_2, padding_2, 0.07323520630598068237, 55, 0.09311912953853607178, 70);
+    // BatchNorm3d(X_stem_2, X_stem_2_num, Mu_stem_4, Var_stem_4, Gamma_stem_4, Bias_stem_4, 0.09311912953853607178, 70, 0.07423608750104904175, 65);
+    // ReLU(X_stem_2, X_stem_2_num, 65);
     
     // // for stem test
     // for(int_t i = 0; i < 3211264; i++)
     //     Y[i] = X_stem_2[i]; // assign result to output
     
     // ========================Sequential 1~4==================================
-    Sequential(X_stem_2, X_seq, 
+    Sequential(X, X_seq, 
         Kernel_seq1_0_conv1_0_0, Kernel_seq1_0_conv1_0_3, Kernel_seq1_0_conv2_0_0, Kernel_seq1_0_conv2_0_3, Kernel_seq1_1_conv1_0_0, Kernel_seq1_1_conv1_0_3, Kernel_seq1_1_conv2_0_0, Kernel_seq1_1_conv2_0_3, 
         Kernel_seq2_0_conv1_0_0, Kernel_seq2_0_conv1_0_3, Kernel_seq2_0_conv2_0_0, Kernel_seq2_0_conv2_0_3, Kernel_seq2_0_downsample_0, Kernel_seq2_1_conv1_0_0, Kernel_seq2_1_conv1_0_3, Kernel_seq2_1_conv2_0_0, Kernel_seq2_1_conv2_0_3,
         Kernel_seq3_0_conv1_0_0, Kernel_seq3_0_conv1_0_3, Kernel_seq3_0_conv2_0_0, Kernel_seq3_0_conv2_0_3, Kernel_seq3_0_downsample_0, Kernel_seq3_1_conv1_0_0, Kernel_seq3_1_conv1_0_3, Kernel_seq3_1_conv2_0_0, Kernel_seq3_1_conv2_0_3,
@@ -296,7 +296,7 @@ void r2plus1d(dtype* X, dtype* Y, dtype* Kernel_stem_0, dtype* Kernel_stem_3, do
         Bais_seq4_0_conv1_0_1, Bais_seq4_0_conv1_1, Bais_seq4_0_conv2_0_1, Bais_seq4_0_conv2_1, Bais_seq4_0_downsample_1, Bais_seq4_1_conv1_0_1, Bais_seq4_1_conv1_1, Bais_seq4_1_conv2_0_1, Bais_seq4_1_conv2_1);
 
     // for sequential test
-    for(int_t i = 0; i < 50176; i++)
+    for(int_t i = 0; i < 3211264; i++)
         Y[i] = X_seq[i]; // assign result to output
     
     // // ======================== AdaptiveAvgPool3d ==================================

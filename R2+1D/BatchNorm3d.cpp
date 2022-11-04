40606a6840525619a693e83bbef5ef95595bea2f
jessie570314 <jessie570314@gmail.com>
2022-11-04 23:44:57 +0800

    Batchnorm and Relu add output

--- R2+1D/BatchNorm3d.cpp
+++ R2+1D/BatchNorm3d.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void BatchNorm3d(dtype* X_data, int_t* X_num, ftype* mu_, ftype* var_, ftype* r, ftype* b, ftype scale_in, int_t zp_in, ftype scale_out, int_t zp_out){
+void BatchNorm3d(dtype* X_data, dtype* Y_data, int_t* X_num, ftype* mu_, ftype* var_, ftype* r, ftype* b, ftype scale_in, int_t zp_in, ftype scale_out, int_t zp_out){
 	int_t N = X_num[0];
 	int_t C = X_num[1];
 	int_t D = X_num[2];
@@ -17,6 +17,7 @@ void BatchNorm3d(dtype* X_data, int_t* X_num, ftype* mu_, ftype* var_, ftype* r,
 				for(int_t h = 0; h < H; h++)
 					for(int_t w = 0; w < W; w++){
 						int_t tmp_X = (int_t)roundf((((ftype((int_t)X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w]-zp_in)*scale_in - mu_[c]) / sqrtf(var_[c]+0.00001)) * r[c] + b[c])/scale_out) + zp_out;
-						X_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w] = (tmp_X > 255) ? 255 : (tmp_X < 0) ? 0 : (dtype)tmp_X;
+						Y_data[n*C*D*H*W + c*D*H*W + d*H*W + h*W + w] = (tmp_X > 255) ? 255 : (tmp_X < 0) ? 0 : (dtype)tmp_X;
 					}
+					
 }

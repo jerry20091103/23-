27307a6d9899f0dbfa1a4e8295b063993ea8d535
plumy2828 <plumy2828@gmail.com>
2022-11-04 20:34:06 +0800

    fix Relu for loop

--- R2+1D/ReLU.cpp
+++ R2+1D/ReLU.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void ReLU(dtype* X_data, int_t* X_num, int_t zp){
 	
-	for(int_t i = X_num[0] * X_num[1] * X_num[2] * X_num[3] * X_num[4]; i >= 0; i--) // traverse all data
+	for(int_t i = X_num[0] * X_num[1] * X_num[2] * X_num[3] * X_num[4]-1; i >= 0; i--) // traverse all data
 		X_data[i] = (X_data[i] > zp) ? X_data[i] : zp;
 	
 	return;

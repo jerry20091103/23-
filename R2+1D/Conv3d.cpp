6060cbceaeb71ad6e82a9231c716223ca4c97aa4
jerry20091103 <jerry20091103@gmail.com>
2022-11-12 00:00:06 +0800

    Move kernel_bram to URAM, change Y to int16_t.

--- R2+1D/Conv3d.cpp
+++ R2+1D/Conv3d.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-void Conv3d(dtype* X_data, int_t* X_num, int_t xi, int_t XC, dtype* Y_data, int_t* Y_num, int_t yi, int_t YC, ktype* Kernel_data, int_t* Kernel_num, int_t* stride, int_t* padding, dtype zp_in)
+void Conv3d(dtype* X_data, int_t* X_num, int_t xi, int_t XC, int16_t* Y_data, int_t* Y_num, int_t yi, int_t YC, ktype* Kernel_data, int_t* Kernel_num, int_t* stride, int_t* padding, dtype zp_in)
 {
 	// get X(input) size
 	int_t KC = X_num[1];
--- R2+1D/r2plus1d.h
+++ R2+1D/r2plus1d.h
@@ -3,8 +3,8 @@
 
 #include <stdint.h>
 // It works!
-typedef int dtype;
-// typedef uint8_t dtype;
+// typedef int dtype;
+typedef uint8_t dtype;
 typedef int int_t;
 typedef float ftype;
 typedef int8_t ktype;
@@ -17,7 +17,7 @@ void r2plus1d(dtype* X, ktype* Kernel_stem_0, ktype* Kernel_stem_3,
             ktype* Kernel_linear,
             dtype* X_stem_1, dtype* X_stem_2, dtype* X_data,dtype* X2_data, dtype* X3_data, dtype* X_seq, dtype* X_adap, dtype* X_linear, 
             dtype* X_tmp_data, dtype* X_batch_data, dtype* X_mid_data);
-void Conv3d(dtype* X_data, int_t* X_num, int_t XI, int_t XC, dtype* Y_data, int_t* Y_num, int_t YI, int_t YC, ktype* Kernel_data, int_t* Kernel_num, int_t* stride, int_t* padding, dtype zp_in);
+void Conv3d(dtype* X_data, int_t* X_num, int_t XI, int_t XC, int16_t* Y_data, int_t* Y_num, int_t YI, int_t YC, ktype* Kernel_data, int_t* Kernel_num, int_t* stride, int_t* padding, dtype zp_in);
 void BatchNorm3d(dtype* X_data, dtype* Y_data, int_t* X_num, ftype* mu_, ftype* var_, ftype* r, ftype* b, ftype scale_in, dtype zp_in, ftype scale_out, dtype zp_out);
 void ReLU(dtype* X_data, dtype* Y_data, int_t* X_num, dtype zp);
 void Conv2Plus1D(dtype* X_data, int_t* X_num, dtype* X_mid_data, dtype* X_batch_data, dtype* X_out_data, int_t* X_out_num, int_t midplanes,

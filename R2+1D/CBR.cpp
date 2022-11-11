330b8c5d636ace5e41a7b350b20334d1196652d9
plumy2828 <plumy2828@gmail.com>
2022-11-11 22:38:08 +0800

    fix CBR.cpp

--- R2+1D/CBR.cpp
+++ R2+1D/CBR.cpp
@@ -22,7 +22,7 @@ void CBR(dtype* X_data, int_t* X_num, int_t XC,
                 X_bram[k] = X_data[xi*XC*X_num[2]*X_num[3]*X_num[4]+k];
 
             if(flag == 0)
-				Conv3d(X_bram, X_num, xi, XC, Y_bram, Y_num, yi, YC, Kernel_bram, Kernel_num, stride, padding, 56);
+				Conv3d(X_bram, X_num, xi, XC, Y_bram, Y_num, yi, YC, Kernel_bram, Kernel_num, stride, padding, conv_zp);
             else if(flag == 1){
 				int_t i = 0;
 				for(int_t yc = 0; yc < YC; yc++)
@@ -30,15 +30,15 @@ void CBR(dtype* X_data, int_t* X_num, int_t XC,
 						for(int_t j = 0; j < Kernel_num[0]*Kernel_num[1]*Kernel_num[2]; j++){
 							Kernel_bram[i++] =  Kernel_data[(yi*YC + yc)*X_num[1]*Kernel_num[0]*Kernel_num[1]*Kernel_num[2] + (xi*XC + xc)*Kernel_num[0]*Kernel_num[1]*Kernel_num[2] + j];
 						}
-				Conv3d_k(X_bram, X_num, xi, XC, Y_bram, Y_num, yi, YC, Kernel_bram, Kernel_num, stride, padding, 56);
+				Conv3d_k(X_bram, X_num, xi, XC, Y_bram, Y_num, yi, YC, Kernel_bram, Kernel_num, stride, padding, conv_zp);
 			}
         }
         
         for(int_t c = 0; c < YC; c++){
             int_t offset = c*Y_num[2]*Y_num[3]*Y_num[4];
             for(int_t k = 0; k < Y_num[2]*Y_num[3]*Y_num[4]; k++){
-                int_t tmp = (int_t)roundf((((Y_bram[offset+k]*3.756307810544967651e-02*Kernel_stem_0_scale[yi*YC+c] - Mu_stem_1[yi*YC+c]) / sqrtf(Var_stem_1[yi*YC+c]+0.00001)) * Gamma_stem_1[yi*YC+c] + Bias_stem_1[yi*YC+c])/0.07323520630598068237);
-                Y_bram[offset+k] = (tmp+55 > 255) ? 255 : (tmp < 0) ? 55 : tmp+55;
+                int_t tmp = (int_t)roundf((((Y_bram[offset+k]*conv_scale*Kernel_stem_0_scale[yi*YC+c] - Mu_stem_1[yi*YC+c]) / sqrtf(Var_stem_1[yi*YC+c]+0.00001)) * Gamma_stem_1[yi*YC+c] + Bias_stem_1[yi*YC+c])/batch_scale);
+                Y_bram[offset+k] = (tmp+batch_zp > 255) ? 255 : (tmp < 0) ? batch_zp : tmp+batch_zp;
                 Y_data[yi*YC*Y_num[2]*Y_num[3]*Y_num[4]+offset+k] = Y_bram[offset+k];
             }
         }

b7d41c25968a6d922199942139a7e65931a086cf
jerry20091103 <jerry20091103@gmail.com>
2022-11-14 02:09:00 +0800

    Optimize CB.cpp and CBR_k.cpp

--- R2+1D/CB.cpp
+++ R2+1D/CB.cpp
@@ -10,21 +10,28 @@ void CB(dtype* X_data, param_t* X_num, int_t XC,
 		dtype conv_in_zp, ftype conv_in_scale, dtype conv_out_zp, ftype conv_out_scale, dtype batch_zp, ftype batch_scale, 
 		ftype* Kernel_scale, ftype* Mu, ftype* Var, ftype* Gamma, ftype* Bias)
 {
+    KERNEL_LOAD_LOOP:
 	for(int_t i = 0; i < Y_num[1]*X_num[1]*Kernel_num[0]*Kernel_num[1]*Kernel_num[2]; i++)
         Kernel_bram[i] = Kernel_data[i];
 	int_t YI = (Y_num[1]%YC) ? (Y_num[1]/YC+1) : (Y_num[1]/YC);
 	int_t XI = (X_num[1]%XC) ? (X_num[1]/XC+1) : (X_num[1]/XC);
+    Y_TILE_LOOP:
     for(int_t yi = 0; yi < YI; yi++){
-        for(int_t k = 0; k < YC*Y_num[2]*Y_num[3]*Y_num[4]; k++)
+        Y_ZERO_LOOP:
+        for(int_t k = 0; k < YC*Y_num[2]*Y_num[3]*Y_num[4]; k++){
+            #pragma HLS UNROLL factor=16
             Y_bram[k] = 0; 
-        
+        }
+        X_TILE_LOOP:
         for(int_t xi = 0; xi < XI; xi++){
+            X_LOAD_LOOP:
             for(int_t k = 0; k < XC*X_num[2]*X_num[3]*X_num[4]; k++)
                 X_bram[k] = X_data[xi*XC*X_num[2]*X_num[3]*X_num[4]+k];
 			Conv3d(X_bram, X_num, xi, XC, Y_bram, Y_num, yi, YC, Kernel_bram, Kernel_num, stride, padding, conv_in_zp);
         }
-        
+        Y_CHANNEL_LOOP:
         for(int_t c = 0; c < YC; c++){
+            BATCH_LOOP:
             int_t offset = c*Y_num[2]*Y_num[3]*Y_num[4];
             for(int_t k = 0; k < Y_num[2]*Y_num[3]*Y_num[4]; k++){
                 int_t tmp = roundf(Y_bram[offset+k]*conv_in_scale*Kernel_scale[yi*YC+c] / conv_out_scale) + conv_out_zp;
